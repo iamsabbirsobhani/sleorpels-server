dba19541454e5aabd75f0d6fa3cc3b440adbd64e
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-02-13 11:42:57 +0600

    query for products with json aggregation

--- app/controller/products.ts
+++ app/controller/products.ts
@@ -8,7 +8,17 @@ interface query {
 }
 
 export const GetProducts = async (req: Request, res: Response) => {
-  const result: query = await db.query("SELECT * FROM Students", []);
+  const result: query = await db.query(
+    `
+    SELECT p.id, p.product_name, p.product_images, p.product_description, p.size_and_fit, p.look_after_me, p.about_me, p.color, p.asking_price, p.discount_pct, json_agg(ps.size) AS sizes
+    FROM products p
+    JOIN products_product_sizes ppm ON p.id = ppm.products_id
+    JOIN product_sizes ps ON ppm.product_sizes_id = ps.id
+    GROUP BY p.id;
+
+    `,
+    []
+  );
 
   if (result.error) {
     console.error("Error executing query: ", result.error.stack);
